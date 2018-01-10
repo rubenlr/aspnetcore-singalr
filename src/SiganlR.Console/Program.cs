918ffe1986312b6b7e69fef5ad52b37201e2a28d
Rúben Lício Reis <rubenlr@gmail.com>
2018-01-10 17:03:16 -0200

    Criando projetos em branco

---
+++ src/SiganlR.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiganlR.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SiganlR.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SiganlR.Core
+{
+    public class Class1
+    {
+    }
+}
